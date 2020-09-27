12f9097005a2a4c6ed6263ba9c6d25704253f990
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2020-09-27 17:04:34 +0300

    Changes so that --disable-dbengine compiles properly

--- collectors/statsd.plugin/statsd.c
+++ collectors/statsd.plugin/statsd.c
@@ -1462,7 +1462,9 @@ static inline RRDSET *statsd_private_rrdset_create(
             , memory_mode     // memory mode
             , history         // history
             , 0               // not archived
+#ifdef ENABLE_DBENGINE
             , NULL
+#endif
     );
     rrdset_flag_set(st, RRDSET_FLAG_STORE_FIRST);
 
@@ -2002,7 +2004,9 @@ static inline void statsd_update_app_chart(STATSD_APP *app, STATSD_APP_CHART *ch
                 , app->rrd_memory_mode      // memory mode
                 , app->rrd_history_entries  // history
                 , 0                         // not archived
+#ifdef ENABLE_DBENGINE
                 , NULL
+#endif
         );
 
         rrdset_flag_set(chart->st, RRDSET_FLAG_STORE_FIRST);
